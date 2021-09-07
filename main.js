66d40bb04a3a1a1fe5b8be0631f59055f2d5ffca
miljkovicjovan <jovan.miljkovic.biz@gmail.com>
2021-09-07 21:10:35 +0300

    finished restructuring and added colors functionality

--- main.js
+++ main.js
@@ -1,49 +1,3 @@
-let grid =  document.getElementById('container');
-
-// the function that creates the grid and add each "square" spot
-createGrid = (cells) => {
-    // appending in each grid box a single square (32x32=1024)
-    for (let i = 0; i < cells * cells; i++) { // this is the default value
-      const div = document.createElement("div");
-      div.classList.add("square"); // add the square styling
-      grid.appendChild(div); // append to each spot
-    }
-};
-
-// target the square element inside each grid spot
-const square = document.querySelector("div");
-// on mouseover change the styling from square to color
-square.addEventListener("mouseover", function(event) {
-  event.target.classList.replace("square", "color");
-});
-
-// create the grid and start the program
-createGrid(32);
-
-
-// -------- SHAKE FUNCTIONALITY --------
-let shake = document.getElementById('shake');
-
-shake.addEventListener('click', e => {
-  grid.innerHTML = ""; // empty the grid
-  grid.style.setProperty("grid-template-columns", `repeat(32, 2fr)`); // change the styling of the grid
-  grid.style.setProperty("grid-template-rows", `repeat(32, 2fr)`);
-  createGrid(32); // call the function as in default
-});
-
-
-// -------- RESIZE FUNCTIONALITY --------
-let resize = document.getElementById('resize');
-
-resize.addEventListener('click', e => {
-  grid.innerHTML = ""; // empty the grid
-  let cells  = prompt("How many cells do you want per side?"); // promt the user for input
-  grid.style.setProperty("grid-template-columns", `repeat(${cells}, 2fr)`); // change the styling of the grid
-  grid.style.setProperty("grid-template-rows", `repeat(${cells}, 2fr)`);
-  createGrid(cells);
-});
-
-
 // -------- DROPDOWN MENU --------
 // to open the dropdown menu
 function openDropdown() {
@@ -53,12 +7,146 @@ function openDropdown() {
 // when user clicks outside of the dropdown menu -> collapse it
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName('dropdown-content');
-    for (let i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      var dropdowns = document.getElementsByClassName('dropdown-content');
+      for (let i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+          }
       }
-    }
   }
 }
+
+// -------- ETCH A SKETCH --------
+
+// object with color modes
+const Modes = Object.freeze({
+  normal: "normal",
+  colors: "colors",
+  greyscale: "greyscale",
+});
+
+function EtchASketch() {
+  const SIDE_LENGTH = 560;
+
+  // default values
+  let cellsPerRow = 10;
+  let mountedAt;
+  let grid;
+  let mode = Modes.normal;
+
+  // when the mode is normal style the cell to have color black
+  function getNormalModeListener(cell) {
+    return function () {
+        cell.style.backgroundColor = "black";
+    }
+  }
+
+  // when the mode is random style the cell to have a random color generated from the randomRGBVal
+  function getColorsModeListener(cell) {
+    function randomRGBVal() {
+        return Math.floor(Math.random() * 256);
+    }
+    return function () {
+        const red = randomRGBVal();
+        const green = randomRGBVal();
+        const blue = randomRGBVal();
+
+        cell.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    }
+  }
+
+  // when the mode is greyscale style the cell to have a black color and each time it is passed a little opacity is added on top 
+  function getGreyscaleModeListener(cell) {
+    let opacity = 0;
+    return function () {
+        opacity += 0.1;
+        cell.style.backgroundColor = "black";
+        cell.style.opacity = opacity;
+    }
+  }
+
+  // function that creates the grid and allows the functions
+  function createDomGrid() {
+    grid = document.createElement('div');
+    grid.setAttribute('id', 'grid');
+
+    for (let i = 0; i < cellsPerRow; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < cellsPerRow; j++) {
+            const cell = document.createElement('div');
+            const cellSideLength = `${(SIDE_LENGTH / cellsPerRow).toFixed(1)}px`;
+
+            cell.classList.add('grid-cell');
+            cell.style.width = cellSideLength;
+            cell.style.height = cellSideLength;
+
+            let listener = getNormalModeListener(cell);
+
+            switch (mode) {
+                case Modes.normal:
+                listener = getNormalModeListener(cell);
+                break;
+                case Modes.colors:
+                listener = getColorsModeListener(cell);
+                break;
+                case Modes.greyscale:
+                listener = getGreyscaleModeListener(cell);
+                break;
+            }
+
+            cell.addEventListener('mouseenter', listener);
+
+            row.append(cell);
+        }
+        grid.append(row);
+    }
+  }
+
+  const reset = (function () {
+    grid.remove();
+    createDomGrid();
+    this.mount(mountedAt);
+  }).bind(this);
+
+  this.changeMode = function (newMode) {
+    mode = newMode;
+    reset();
+  }
+
+  this.mount = function (selector) {
+    mountedAt = selector;
+    document.querySelector(selector).append(grid);
+  }
+
+  this.shake = function () {
+    cellsPerRow = prompt('How many cells per side?');
+    reset();
+  }
+
+  createDomGrid();
+}
+
+let etchASketch = new EtchASketch();
+etchASketch.mount('#container');
+
+document.querySelector('#shake').addEventListener('click', () => {
+  etchASketch.shake();
+});
+
+document.querySelector('#resize').addEventListener('click', () => {
+  etchASketch.shake();
+});
+
+document.querySelector('#normal').addEventListener('click', () => {
+  etchASketch.changeMode(Modes.normal);
+})
+
+document.querySelector('#random').addEventListener('click', () => {
+  etchASketch.changeMode(Modes.colors);
+})
+
+document.querySelector('#monochrome').addEventListener('click', () => {
+  etchASketch.changeMode(Modes.greyscale);
+})
